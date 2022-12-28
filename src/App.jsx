d5058d4bdd4167c69f791d63dd14dc8ee61db072
Dob <lalilulelo.mg@gmail.com>
2022-12-28 16:32:49 +1100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-
-  return (
-    <div>
-      
-    </div>
-  )
+	return <div></div>
 }
 
 export default App
