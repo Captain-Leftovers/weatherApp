9db56a9a01d51dced71471f0d7431d3d2621882b
Dob <lalilulelo.mg@gmail.com>
2022-12-30 15:21:40 +1100

    added svg for location and light dark

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,24 @@
+import LightDarkSvg from "./assets/LightDarkSvg"
+import LocationSvg from "./assets/LocationSvg"
+
 
 export default function Header (){
   
    
 return (
-<h1>Header</h1>
+ <div className="headerContainer">
+    <div className="locationWrap">
+        <p className="locationText">
+            (Location Here)
+        </p>
+    </div>
+    <div className="inputWrapper">
+        <input type="text" />
+    </div>
+    <div className="locationSvgWrapper">
+        <LocationSvg/>
+        <LightDarkSvg/>
+    </div>
+ </div>
 )
 }
--- src/components/HourlyCard/HourlyCard.jsx
+++ src/components/HourlyCard/HourlyCard.jsx
@@ -4,6 +4,19 @@ export default function HourlyCard (){
   
    
 return (
-<h2>Hourly Card</h2>
+    <div className="cardContainer">
+        <div className="time">(10:30)</div>
+        <div className="secondRow">
+            <div className="deg">(28&deg;C)</div>
+            <div className="dayDescription">(Sunny)</div>
+            <div className="icon">(icon)</div>
+        </div>
+        <div className="thirdRow">
+            <div className="rf">(RF:30)</div>
+            <div className="humidity">(RF:30)</div>
+            <div className="RF">(RF:30)</div>
+            <div className="RF">(RF:30)</div>
+        </div>
+    </div>
 )
 }
