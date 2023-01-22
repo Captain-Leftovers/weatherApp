0cf125bcb44ffd8d416e50232e55133c78202379
Dob <lalilulelo.mg@gmail.com>
2023-01-23 07:39:37 +1100

    slider index passed to carousel component

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,13 +2,20 @@ import style from './Carousel.module.css'
 import DailyCard from "../DailyCard/DailyCard.jsx"
 
 export default function Carousel ({days = []}) {
+
+let sliderIndex = { 
+  '--slider-index': 0,
+}
+
+
+
 return (
     
     <div className={style.container}>
       <button className={`${style.handle} ${style.leftHandle}`} >
         <div className={style.text}>&#8249;</div>
       </button>
-      <div className={style.slider}>
+      <div  style={sliderIndex}   className={style.slider}>
         {days}
       </div>
       <button className={`${style.handle} ${style.rightHandle}`}>
